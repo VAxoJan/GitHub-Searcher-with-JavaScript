d949a064dba215be9ca9d335d2fff4c69f503357
vaxo <vaxojaniashvili186@gmail.com>
2024-02-15 23:17:09 +0400

    finishing-project

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+import { fetchGitHubUserData } from "./styles.js";
+
+const search = document.getElementById("search");
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", () => {
+  if (search.value === "") {
+    search.placeholder = "No result";
+    search.style.color = "red";
+    search.style.fontWeight = "700";
+  } else {
+    searchButton.click();
+    const searchValue = search.value;
+    search.value = "";
+    fetchGitHubUserData(searchValue);
+    search.placeholder = "Search GitHub Username...";
+    search.style.color = "#4B6A9B";
+    search.style.fontWeight = "400";
+  }
+});
+
+search.addEventListener("focusin", () => {
+  search.placeholder = "Search GitHub Username...";
+  search.style.color = "#4B6A9B";
+  search.style.fontWeight = "400";
+});
+
+search.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    searchButton.click();
+  }
+});
+
+const toggle = document.getElementById("darkmode");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  
+});
